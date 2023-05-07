f060334b70f04298ca3e889c7a31e3b38213747e
kopulovskiy <132813572+kopulovskiy@users.noreply.github.com>
2023-05-07 22:41:59 +0300

    Add files via upload

---
+++ sortirovka.py
@@ -0,0 +1,6 @@
+numbers = [5, 2, 8, 1, 9, 3]
+ascending_order = sorted (numbers)
+descending_order = sorted (numbers, reverse=True)
+
+print ("vozrast", ascending_order)
+print ("ubivaniye", descending_order)
